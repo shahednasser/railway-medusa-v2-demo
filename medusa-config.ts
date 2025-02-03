919bb1e09e0e541b15a84418d37453c1394a53fc
Shahed Nasser <shahednasser@gmail.com>
2025-02-03 11:01:29 +0200

    add backendUrl to admin

--- medusa-config.ts
+++ medusa-config.ts
@@ -17,6 +17,7 @@ module.exports = defineConfig({
   },
   admin: {
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
   },
   modules: [
     {
