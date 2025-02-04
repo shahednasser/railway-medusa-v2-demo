279af91f0e835580f9781d49225c90fa41f7f18b
Shahed Nasser <shahednasser@gmail.com>
2025-02-04 16:43:04 +0200

    add date

--- src/jobs/test.ts
+++ src/jobs/test.ts
@@ -1,12 +1,12 @@
-// import {
-//   MedusaContainer
-// } from "@medusajs/types";
+import {
+  MedusaContainer
+} from "@medusajs/types";
 
-// export default async function myCustomJob(container: MedusaContainer) {
-//   console.log("Greetings!")
-// }
+export default async function myCustomJob(container: MedusaContainer) {
+  console.log("Greetings!", new Date())
+}
 
-// export const config = {
-//   name: "daily-product-report",
-//   schedule: "*/5 * * * *", // Every day at midnight
-// };
+export const config = {
+  name: "daily-product-report",
+  schedule: "*/5 * * * *", // Every day at midnight
+};
